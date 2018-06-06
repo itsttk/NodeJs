add36aafa0b526bcb594b485a8f5d76bf4bc8fcf
itsttk <tkt160230@utdallas.edu>
2018-06-06 18:54:40 -0400

    first project

---
+++ note-app/app.js
@@ -0,0 +1,12 @@
+console.log('Starting app!');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+
+const notes = require('./notes.js');
+
+
+
+
+
---
+++ note-app/notes.js
@@ -0,0 +1,14 @@
+console.log("starting notes.js ")
+
+//console.log(module);
+
+module.exports.age=25
+
+module.exports.addNote =  ()=>{
+
+	console.log('add Note')
+
+	return 'new Note';
+
+};
+
