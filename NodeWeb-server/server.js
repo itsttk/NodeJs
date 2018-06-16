cfaf045d06d3076215e0d5f3215acaa87d038b27
itsttk <tkt160230@utdallas.edu>
2018-06-15 23:54:03 -0400

    first webserver project' '

---
+++ NodeWeb-server/server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view-engine', 'hbs');
+
+
+app.use((req, res, next)=>{
+
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+
+	fs.appendFile('server.log', log + '\n' , (err)=>{
+
+		if(err){
+			console.log('unable to append file');		}
+
+	});
+
+	next();
+
+});
+
+app.use((req,res,next)=>{
+
+	res.render('maintain.hbs');
+
+});
+
+app.use (express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIT',(text)=>{
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+
+	res.render('home.hbs', {
+	title: 'welcome page',
+   	welcome: 'welcome to my page'
+
+
+	});
+
+
+});
+
+app.get('/about',(req,res)=>{
+
+   res.render('about.hbs',{
+   	title: 'About page',
+   });
+});
+
+
+app.get('/bad',(req,res)=>{
+
+	res.send({
+		errorMessage: 'unable to handle request'
+	});
+
+});
+
+
+app.listen(3000,()=>{
+	console.log('server is up and running');
+});
